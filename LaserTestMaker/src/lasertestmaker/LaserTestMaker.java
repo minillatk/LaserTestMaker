c13fd104dc1216c531021878f02cc241cea6378d
minillatk <kimuratadashi@192.168.10.107>
2017-09-21 00:30:02 +0900

    Merge origin/170113_01 into 170113_01

--- LaserTestMaker/src/lasertestmaker/LaserTestMaker.java
+++ LaserTestMaker/src/lasertestmaker/LaserTestMaker.java
@@ -10,23 +10,25 @@ package lasertestmaker;
  * @author minillatk
  */
 public class LaserTestMaker {
-    public final String GCODE_LASER_ON = "M3";//"M106"; //レーザーONのG-code命令
-    public final String GCODE_LASER_OFF = "M3"; //レーザーOFFのG-code命令
+    public final String GCODE_LASER_ON = "M04";//"M106"; //レーザーONのG-code命令
+    public final String GCODE_LASER_OFF = "M05"; //レーザーOFFのG-code命令
     public int laser_power; //レーザーの出力0-255 標準出力8W=136 (＊最高出力15W=255 使用不可)
-    public double X_default, Y_default, Z_default;  //デフォルト値
+    public double X_default, Y_default, Z_default;  //デフォルト位置
     public double X_MV, Y_MV, Z_MV, F_MV; //移動量 例＞10 = 10mm間隔で移動
+    public double Laser_F_MV; //レーザー出力時の移動量
     public double Laser_line_width;//レーザーで焼き付ける線の長さ
     
     
     LaserTestMaker(){ //コンストラクタ
-        this.laser_power = 255;
+        this.laser_power = 500;
         this.X_default = 0;
         this.Y_default = 0;
         this.Z_default = 0;
         this.X_MV = 0;
         this.Y_MV = 5;
         this.Z_MV = 0.25;
-        this.F_MV = 500;
+        this.F_MV = 1000;
+        this.Laser_F_MV = 500;
         this.Laser_line_width = 30;
         
     }
@@ -45,19 +47,20 @@ public class LaserTestMaker {
         
         StringBuilder sb = new StringBuilder();
         sb.append("          ;メインスタート\n ");
+        sb.append(GCODE_LASER_ON + " S" + this.laser_power + "      ;レーザーON\n ");
         sb.append("G0 F" + F_MV + "\n ");
         sb.append("G0 " + "X" + this.X_default + " Y" + this.Y_default + "\n ");
+        
         for(int i=0 ;i<10;i++){
-            sb.append(GCODE_LASER_ON + " S" + this.laser_power + "      ;レーザーON\n ");
-            sb.append("G0 " + "X" + x + " Y" + y + ";in\n ");
+            sb.append("G1 " + "X" + x + " Y" + y + " F" + Laser_F_MV + ";in\n ");
             x += Laser_line_width;
-            sb.append("G0 " + "X" + x + " Y" + y + ";out\n ");
+            sb.append("G1 " + "X" + x + " Y" + y + " F" + Laser_F_MV + ";out\n ");
             x -= Laser_line_width;
-            sb.append(GCODE_LASER_OFF + " S1        ;レーザーOFF\n ");
             y += this.Y_MV;
             z += this.Z_MV;
             sb.append("G0 " + "X" + x + " Y" + y + " Z" + z + ";Y移動\n ");
         }
+
         sb.append("\n          ;メインここまで");
         return sb.toString();
     }
@@ -73,9 +76,8 @@ public class LaserTestMaker {
     String footerScript(){
         String footer = 
                 GCODE_LASER_OFF + " S1\n" +
-                "G0 F3000\n" +
-                "G0 X0 Y0 Z0\n" +
-                "M18        ;すべてのステッパーモーターの電源をオフまたは回転をオフ（すべてのステッパーモーターを無効にする）";
+                "G90 X0 Y0 Z0\n" +
+                "M30       ;終了コード";
     return footer;
     }
 
